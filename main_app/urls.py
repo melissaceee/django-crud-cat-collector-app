df77982b7412c96d8684bf509c3cf17f47fa25d4
melissaceee <cheng.melissa@outlook.com>
2024-11-07 20:56:01 -0500

    update cat collector

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+  path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  # route for cats index
+  path('cats/', views.cat_index, name='cat-index'),
 ]
